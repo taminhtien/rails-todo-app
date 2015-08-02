d29d9ecfe56d2485fdab1d0677466e202506ec78
Tien Ta <taminhtien1993@gmail.com>
2015-08-02 13:57:41 +0700

    Add complete action & edit todo item view

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,12 +1,13 @@
 class TodoItemsController < ApplicationController
 	before_action :set_todo_list
+	before_action :set_todo_item, except: [:create]
+
 	def create
 		@todo_item = @todo_list.todo_items.create(todo_item_params)
 		redirect_to @todo_list
 	end
 
 	def destroy
-		@todo_item = @todo_list.todo_items.find(params[:id])
 		if @todo_item.destroy
 			flash[:success] = "Todo List Item was deleted"
 		else
@@ -15,10 +16,19 @@ class TodoItemsController < ApplicationController
 		redirect_to @todo_list
 	end
 
+	def complete
+		@todo_item.update_attribute(:completed_at, Time.now)
+		redirect_to @todo_list, notice: "Todo Item complete"
+	end
+
 	private
 
 		def set_todo_list
 			@todo_list = TodoList.find(params[:todo_list_id])
+		end
+
+		def set_todo_item
+			@todo_item = @todo_list.todo_items.find(params[:id])
 		end
 
 		def todo_item_params
